f3fe2324111d06244a975201a5ff205fa1353429
d1maall <d1maall1606@gmail.com>
2015-04-29 20:11:04 +0300

    слайды-слайды

--- script.js
+++ script.js
@@ -3,7 +3,23 @@
  */
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        navigation: true,
-        scrollOverflow: true
+        navigation: false,
+        scrollOverflow: true,
+        afterRender: function(){
+            $(".three-one-content").css("width", $(window).width() - 50);
+            $(".three-one-content").css("left", 25);
+            $(".three-one-content").css("height", $(window).height() - 150);
+            $(".three-one-content").css("top", 125);
+
+            $(".three-two-content").css("width", $(window).width() - 50);
+            $(".three-two-content").css("left", $(window).width() + 25);
+            $(".three-two-content").css("height", $(window).height() - 150);
+            $(".three-two-content").css("top", 125);
+
+            $(".three-three-content").css("width", $(window).width() - 50);
+            $(".three-three-content").css("left", 2 * $(window).width() + 25);
+            $(".three-three-content").css("height", $(window).height() - 150);
+            $(".three-three-content").css("top", 125);
+        }
     });
 });
