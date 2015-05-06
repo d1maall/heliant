631b3ecff3c702bfe09eb473055b4f7b56a252d5
fasja <daneil ford11>
2015-05-06 17:50:17 +0300

    all slaids

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ $(document).ready(function() {
         navigation: false,
         scrollOverflow: true,
         afterRender: function(){
-            var titleHeight = $(".three-title").height();
+            var titleHeight = $(".three-row-1").height();
 
             $(".three-one-content").css("width", $(window).width() - 150);
             $(".three-one-content").css("left", 75);
             $(".three-one-content").css("min-height", $(window).height() - 170);
-            $(".three-one-content").css("top", titleHeight);
+            $(".three-one-content").css("top", 0);
 
             $(".three-two-content").css("width", $(window).width() - 150);
             $(".three-two-content").css("left", $(window).width() + 75);
             $(".three-two-content").css("min-height", $(window).height() - 170);
-            $(".three-two-content").css("top", titleHeight);
+            $(".three-two-content").css("top", 0);
 
             $(".three-three-content").css("width", $(window).width() - 150);
             $(".three-three-content").css("left", 2 * $(window).width() + 75);
             $(".three-three-content").css("min-height", $(window).height() - 170);
-            $(".three-three-content").css("top", titleHeight);
+            $(".three-three-content").css("top", 0);
 
-            setInterval(function () {
+            /*setInterval(function () {
                 $.fn.fullpage.moveSlideRight();
-            }, 4000);
+            }, 4000);*/
         }
     });
 });
